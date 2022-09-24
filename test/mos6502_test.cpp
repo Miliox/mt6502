da2bdc6f7e0cbd6ab2ef42eabec0b53ad83b52fb
Emiliano Firmino <emiliano@frmn.se>
2022-09-24 20:49:44 +0200

    Test set flags
    Signed-off-by: Emiliano Firmino <emiliano@frmn.se>

--- test/mos6502_test.cpp
+++ test/mos6502_test.cpp
@@ -6,6 +6,8 @@
 
 #include "mos6502/bus.hpp"
 #include "mos6502/cpu.hpp"
+#include "mos6502/regs.hpp"
+#include "mos6502/status.hpp"
 
 class MockBus final : public mos6502::IBus {
 public:
@@ -27,15 +29,83 @@ public:
     }
 
 private:
-    std::unordered_map<std::uint16_t, std::uint8_t> mem_map;
+    std::unordered_map<std::uint16_t, std::uint8_t> mem_map{};
 };
 
 MockBus::MockBus() = default;
 
 MockBus::~MockBus() = default;
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    auto mock_bus = std::shared_ptr<MockBus>(new MockBus{});
+class CpuFixture {
+public:
+    CpuFixture();
 
+protected:
+    std::shared_ptr<MockBus> mock_bus{new MockBus{}};
     mos6502::Cpu cpu{mock_bus};
+};
+
+CpuFixture::CpuFixture() {
+    mock_bus->mockAddressValue(0x01, 0x00);
+    mock_bus->mockAddressValue(0x02, 0x00);
+}
+
+TEST_CASE_METHOD(CpuFixture, "Instruction Test", "[SEC]") {
+    mock_bus->mockAddressValue(0x00, 0x38);
+
+    REQUIRE(cpu.step() == 2U);
+    REQUIRE(cpu.regs().pc == 1U);
+    REQUIRE(cpu.regs().sr == mos6502::Status::C);
+}
+
+TEST_CASE_METHOD(CpuFixture, "Instruction Test", "[SED]" ) {
+    mock_bus->mockAddressValue(0x00, 0xF8);
+
+    REQUIRE(cpu.step() == 2U);
+    REQUIRE(cpu.regs().pc == 1U);
+    REQUIRE(cpu.regs().sr == mos6502::Status::D);
+}
+
+TEST_CASE_METHOD(CpuFixture, "Instruction Test", "[SEI]" ) {
+    mock_bus->mockAddressValue(0x00, 0x78);
+
+    REQUIRE(cpu.step() == 2U);
+    REQUIRE(cpu.regs().pc == 1U);
+    REQUIRE(cpu.regs().sr == mos6502::Status::I);
+}
+
+TEST_CASE_METHOD(CpuFixture, "Instruction Test", "[CLC]" ) {
+    mock_bus->mockAddressValue(0x00, 0x18);
+
+    cpu.regs().sr = 0xFF;
+    REQUIRE(cpu.step() == 2U);
+    REQUIRE(cpu.regs().pc == 1U);
+    REQUIRE((cpu.regs().sr & mos6502::Status::C) == 0);
+}
+
+TEST_CASE_METHOD(CpuFixture, "Instruction Test", "[CLD]" ) {
+    mock_bus->mockAddressValue(0x00, 0xD8);
+
+    cpu.regs().sr = 0xFF;
+    REQUIRE(cpu.step() == 2U);
+    REQUIRE(cpu.regs().pc == 1U);
+    REQUIRE((cpu.regs().sr & mos6502::Status::D) == 0);
+}
+
+TEST_CASE_METHOD(CpuFixture, "Instruction Test", "[CLI]" ) {
+    mock_bus->mockAddressValue(0x00, 0x58);
+
+    cpu.regs().sr = 0xFF;
+    REQUIRE(cpu.step() == 2U);
+    REQUIRE(cpu.regs().pc == 1U);
+    REQUIRE((cpu.regs().sr & mos6502::Status::I) == 0);
+}
+
+TEST_CASE_METHOD(CpuFixture, "Instruction Test", "[CLV]" ) {
+    mock_bus->mockAddressValue(0x00, 0xB8);
+
+    cpu.regs().sr = 0xFF;
+    REQUIRE(cpu.step() == 2U);
+    REQUIRE(cpu.regs().pc == 1U);
+    REQUIRE((cpu.regs().sr & mos6502::Status::V) == 0);
 }
