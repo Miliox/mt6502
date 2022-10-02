a8f44bd469bb9bbe12972836d6ddeefc749b81a1
Emiliano Firmino <emiliano@frmn.se>
2022-10-02 13:43:13 +0200

    Move benchmark to tools
    Signed-off-by: Emiliano Firmino <emiliano@frmn.se>

--- tools/bench/mos6502_bench.cpp
+++ tools/bench/mos6502_bench.cpp
@@ -5,6 +5,7 @@
 #include <limits>
 #include <cmath>
 #include <memory>
+#include <string_view>
 
 #include "mos6502/bus.hpp"
 #include "mos6502/cpu.hpp"
@@ -36,33 +37,53 @@ private:
 
 BenchBus::~BenchBus() = default;
 
-static double micro_bench(std::uint8_t opcode) {
+static double micro_bench(std::uint8_t opcode, std::uint64_t number_of_runs) {
+    assert(number_of_runs > 0);
+
     std::shared_ptr<BenchBus> bus{new BenchBus{opcode}};
     mos6502::Cpu cpu{bus};
 
     auto const t0 = std::chrono::steady_clock::now();
-    for (size_t i{}; i < 1'000'000'000U; ++i)
+    for (std::uint64_t i{}; i < number_of_runs; ++i)
     {
         cpu.step();
     }
     auto const t1 = std::chrono::steady_clock::now();
     auto const delta = std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0);
-    double const average = delta.count() / 1e9;
+    double const average = delta.count() / static_cast<double>(number_of_runs);
 
-    printf("%02x => %.18f\n", opcode, average);
+    printf("%02x => %.18f ns\n", opcode, average);
     fflush(stdout);
 
     return average;
 }
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " number_of_runs\n";
+        return 1;
+    }
+
+    std::string_view arg_1{argv[1]};
+    if (!std::all_of(arg_1.begin(), arg_1.end(), [](char c) { return std::isdigit(c); }))
+    {
+        std::cerr << "Error: number_of_runs must be a positive number\n";
+        return 2;
+    }
+
+    std::uint64_t number_of_runs = std::stoull(arg_1.data());
+    if (number_of_runs == 0) {
+        std::cerr << "Error: number_of_runs must be a positive number\n";
+        return 2;
+    }
+
     std::array<double, 256> instr_avg{};
 
     std::cout << "Instruction Average Duration:" << std::endl;
     for (size_t opcode{}; opcode < instr_avg.size(); ++opcode)
     {
         try {
-            instr_avg[opcode] = micro_bench(static_cast<std::uint8_t>(opcode));
+            instr_avg[opcode] = micro_bench(static_cast<std::uint8_t>(opcode), number_of_runs);
         } catch(...) {
             instr_avg[opcode] = nan("");
         }
